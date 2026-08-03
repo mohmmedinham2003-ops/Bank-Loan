bc722076afe8acb8fd394dab62580c0164dbd5ab
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-08-03 12:00:40 +0530

    Enhance login and registration forms with improved input fields and add customer registration functionality

---
+++ register.js
@@ -0,0 +1,47 @@
+function registerCustomer(){
+    let userId = 0;
+    userId++;
+
+     let customer = {
+        
+        userName: document.getElementById("txtInputUserName").value,
+        emailId: document.getElementById("txtInputEmail1").value,
+        fullName: document.getElementById("txtInputName").value,
+        password: document.getElementById("txtInputPassword").value
+    };
+
+
+
+    fetch("https://api.freeprojectapi.com/api/BankLoan/RegisterCustomer",{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify(customer)
+
+    })
+    .then(res=>res.json())
+    .then(result=>{
+        console.log(result);
+
+        if(result.result){
+            alert("SingIn successfull");
+            localStorage.setItem("customer",JSON.stringify(result.data));
+            window.location = "login.html";
+
+        }else{
+            alert(result.message);
+        }
+        
+
+
+    })
+        .catch(error => console.log(error));
+
+
+}
+
+

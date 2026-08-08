6e8299a864848a9a216fe40516ea9fcd2a323d41
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-08-08 11:27:11 +0530

    Implement getAllApplications function to fetch and display loan applications

---
+++ employeeInterface.js
@@ -0,0 +1,40 @@
+function getAllApplications() {
+
+    fetch("https://api.freeprojectapi.com/api/BankLoan/GetAllApplications").then(res => res.json()).then(result => {
+
+        console.log(result);
+
+
+        result.data.forEach(application => {
+
+            document.getElementById("tblApplications").innerHTML += `
+                <tr>
+
+                    <td>${application.applicationId}</td>
+
+                    <td>${application.fullName}</td>
+
+                    <td>${application.loanAmount}</td>
+
+                    <td>${application.applicationStatus}</td>
+
+                    <td>
+
+                        <button onclick="approveLoan(${application.applicationId})">
+                            Approve
+                        </button>
+
+                        <button onclick="rejectLoan(${application.applicationId})">
+                            Reject
+                        </button>
+
+                    </td>
+
+                </tr>
+            `;
+
+        });
+
+    });
+
+}
